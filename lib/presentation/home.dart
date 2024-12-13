9b63020ce90f32614917cae9dd0f2bd1c6dd046c
Alejo <alejandrovanegas09@outlook.com>
2024-12-13 12:54:50 -0500

    s

--- lib/presentation/home.dart
+++ lib/presentation/home.dart
@@ -21,7 +21,7 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int _counter = 0;
 
-  void _incrementCounter() {
+  void _incrementCounter() {a
     setState(() {
       // This call to setState tells the Flutter framework that something has
       // changed in this State, which causes it to rerun the build method below
